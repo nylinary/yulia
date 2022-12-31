e00b0659609e65302f63498ff501cf51b3642faa
nylinary <n.sisoev@energy.digital>
2022-12-31 03:42:01 +0300

    No reasons bugfi

--- reasons/views.py
+++ reasons/views.py
@@ -7,22 +7,20 @@ from django.template import loader
 
 def index(request):
     template = "reasons/reasons.html"
-    try:
-        random_reason = models.Reason.objects.order_by("?")[0]
-    except models.Reason.DoesNotExist:
+    random_reason = models.Reason.objects.order_by("?")
+    if not random_reason:
         return render(request, template, {"random_reason": None})
-    context = {"random_reason": random_reason}
+    context = {"random_reason": random_reason[0]}
     return render(request, template, context)
 
 def ajax_get_reason(request):
-    try:
-        random_reason = models.Reason.objects.order_by("?")[0]
-    except models.Reason.DoesNotExist:
+    random_reason = models.Reason.objects.order_by("?")
+    if not random_reason:
         return JsonResponse({
             "success": False,
             })
     return JsonResponse({
             "success": True,
-            "reason_name": random_reason.name,
-            "reason_description": random_reason.description
+            "reason_name": random_reason[0].name,
+            "reason_description": random_reason[0].description
         })
